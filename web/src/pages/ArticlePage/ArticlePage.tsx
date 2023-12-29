457d72039d528beae01e9c1609a46a2c3ae64bef
xmyoot <xmyoot@yahoo.com>
2023-12-28 16:01:26 -0800

    setup first pages, layout, all posts cell, and individual articles page and routing

---
+++ web/src/pages/ArticlePage/ArticlePage.tsx
@@ -0,0 +1,14 @@
+import { MetaTags } from '@redwoodjs/web'
+import { Link, routes } from '@redwoodjs/router'
+
+const ArticlePage = () => {
+  return (
+    <>
+      <MetaTags title="Article" description="Article page" />
+
+
+    </>
+  )
+}
+
+export default ArticlePage
